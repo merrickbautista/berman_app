dd9eba16ac564ebce59d45ce86a81dcfb70b12b8
Merrick Bautista <merrickbautista@gmail.com>
2015-10-24 11:41:18 -0400

    Fixed Calendar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,9 @@ class StaticPagesController < ApplicationController
 
   def booking
   	@date = Date.today
+    @week = (@date.beginning_of_week..@date.end_of_week).to_a
+    @days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    @times = ["10:00AM","12:00PM","1:00PM","2:00PM"]
     @option = params[:option]
   end
 
